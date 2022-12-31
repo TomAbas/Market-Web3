7f153fcfde1ad0e419d1d47b7e13d4246388edb1
Bekh-nam <bekhnam2021@gmail.com>
2022-12-31 15:10:07 +0700

    deploy aptos

---
+++ build/static/js/563.f43ce967.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmetaspacecy_xmas=self.webpackChunkmetaspacecy_xmas||[]).push([[563],{36563:function(){}}]);
---
+++ build/static/js/942.f12ffd4e.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunkmetaspacecy_xmas=self.webpackChunkmetaspacecy_xmas||[]).push([[942],{24654:function(){},52361:function(){},94616:function(){}}]);
