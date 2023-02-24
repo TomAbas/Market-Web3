72e143127588b22422b6bde1e87629e967e2f86a
TomAbas <tructam37@gmail.com>
2023-02-24 11:09:58 +0700

    form auction

--- src/components/Marketplace/SellItemPage/TabSellMethod/TabSellMethod.tsx
+++ src/components/Marketplace/SellItemPage/TabSellMethod/TabSellMethod.tsx
@@ -1,10 +1,12 @@
-import FormSellFixPrice from 'components/Forms/FormSellFixPrice/FormSellFixPrice';
+// import FormSellFixPrice from 'components/Forms/FormSellFixPrice/FormSellFixPrice';
 import TabCommon from 'customComponents/customTabs/TabCommon/TabCommon';
 import iconAssetBlack from '../../../../assets/icons/asset-black.webp';
 import iconAssetBlue from '../../../../assets/icons/asset-blue.webp';
 import iconHistoryBlack from '../../../../assets/icons/history-black.webp';
 import iconHistoryBlue from '../../../../assets/icons/history-blue.webp';
-import FormCreateAuction from 'components/Forms/FormCreateAuction/FormCreateAuction';
+// import FormCreateAuction from 'components/Forms/FormCreateAuction/FormCreateAuction';
+import SellFixPrice from '../SellFixPrice/SellFixPrice';
+import SellAuction from '../SellAuction/SellAuction';
 
 const TabSellMethod = () => {
 	const tabsDetail = {
@@ -13,7 +15,7 @@ const TabSellMethod = () => {
 				title: 'Fixed Price',
 				icon: <img src={iconAssetBlack} alt="asset icon" width={20} height={20} />,
 				iconSelected: <img src={iconAssetBlue} alt="asset icon" width={20} height={20} />,
-				isShow: false,
+				isShow: true,
 			},
 			{
 				title: 'Auction',
@@ -21,15 +23,15 @@ const TabSellMethod = () => {
 				iconSelected: (
 					<img src={iconHistoryBlue} alt="history icon" width={20} height={20} />
 				),
-				isShow: false,
+				isShow: true,
 			},
 		],
 		sections: [
 			{
-				Section: <FormSellFixPrice />,
+				Section: <SellFixPrice />,
 				isShow: true,
 			},
-			{ Section: <FormCreateAuction />, isShow: false },
+			{ Section: <SellAuction />, isShow: true },
 		],
 	};
 	return (
